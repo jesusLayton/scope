488c3c4956aaf96d8a7fdd0fec34d5780fdda74a
jesusLayton <jesuslayton92@gmail.com>
2022-08-21 22:44:40 -0500

    commit inicial

---
+++ src/scope/funtion.js
@@ -0,0 +1,13 @@
+function saludos(){
+    let user='ana';
+    console.log(user);
+
+    if (user === 'ana') {
+        console.log(`hello ${user}`)
+        
+    }
+}
+
+saludos();
+
+console.log(user)
---
+++ src/scope/global.js
@@ -0,0 +1,15 @@
+var fruit  = 'apple';
+
+function best(){
+    console.log(fruit)
+}
+
+best();
+
+function paises(){
+    contry='colombia';
+    console.log(contry);
+}
+
+paises();
+console.log(contry);
